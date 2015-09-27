da4fa0c350ed3ac2ec6c89c061810f31efe47355
Craig A. Stockton <craig.a.stockton@gmail.com>
2015-09-27 08:09:37 -0700

    UiView - removed getRelativeUri()

--- src/main/java/com/craigstockton/uinavigator/UiView.java
+++ src/main/java/com/craigstockton/uinavigator/UiView.java
@@ -5,8 +5,6 @@ public abstract class UiView {
     private final UiElement viewElement;
     private final String viewUri;
 
-    public abstract String getRelativeUri();
-
     protected UiView(String viewUri, UiElement viewElement) {
         this.viewUri = viewUri;
         this.viewElement = viewElement;
