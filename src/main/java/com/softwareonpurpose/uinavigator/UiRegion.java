2de224926e1ad61c11b41fb362c9933b223d7a53
Craig A. Stockton <craig.a.stockton@gmail.com>
2016-09-09 11:51:37 -0700

    Reverted previous change

--- src/main/java/com/softwareonpurpose/uinavigator/UiRegion.java
+++ src/main/java/com/softwareonpurpose/uinavigator/UiRegion.java
@@ -17,7 +17,7 @@ public abstract class UiRegion {
         return regionElement.waitUntilVisible();
     }
 
-    public void suppressConstructionLogging(boolean suppress) {
+    public static void suppressConstructionLogging(boolean suppress) {
         suppressLogging = suppress;
     }
 
