a6285e10a3bf7d2dc24954315fba644e1be84750
Craig A. Stockton <craig.a.stockton@gmail.com>
2016-09-21 16:54:10 -0700

    Added an override to UiView.load() to allow appending a relative uri

--- src/main/java/com/softwareonpurpose/uinavigator/UiView.java
+++ src/main/java/com/softwareonpurpose/uinavigator/UiView.java
@@ -3,17 +3,22 @@ package com.softwareonpurpose.uinavigator;
 public abstract class UiView {
 
     private final UiElement viewElement;
-    private final String viewUri;
+    private final String VIEW_URI;
 
     protected UiView(String viewUri, UiElement viewElement) {
-        this.viewUri = viewUri;
+        this.VIEW_URI = viewUri;
         this.viewElement = viewElement;
     }
 
     protected abstract boolean confirmElementStates();
 
     protected void load() {
-        UiHost.getInstance().load(viewUri);
+        UiHost.getInstance().load(VIEW_URI);
+    }
+
+    protected void load(String relativeUri) {
+        String explicitUri = String.format("%s/%s", VIEW_URI, relativeUri);
+        UiHost.getInstance().load(explicitUri);
     }
 
     protected UiElement getElement() {
