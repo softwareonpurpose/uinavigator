a0e016a61889c65d936dcc7a9ff80f91a85249dd
Stockton <Craig.Stockton@nike.com>
2017-07-20 20:33:10 -0700

    Handle querystring extensions of a url

--- src/main/java/com/softwareonpurpose/uinavigator/UiView.java
+++ src/main/java/com/softwareonpurpose/uinavigator/UiView.java
@@ -3,21 +3,22 @@ package com.softwareonpurpose.uinavigator;
 public abstract class UiView {
 
     private final UiElement viewElement;
-    private final String VIEW_URI;
+    private final String viewUri;
 
     protected UiView(String viewUri, UiElement viewElement) {
-        this.VIEW_URI = viewUri;
+        this.viewUri = viewUri;
         this.viewElement = viewElement;
     }
 
     protected abstract boolean confirmElementStates();
 
     protected void load() {
-        UiHost.getInstance().load(VIEW_URI);
+        UiHost.getInstance().load(viewUri);
     }
 
     protected void load(String relativeUri) {
-        String explicitUri = String.format("%s/%s", VIEW_URI, relativeUri);
+        relativeUri = relativeUri.substring(0, 1).equals("?") ? relativeUri : String.format("/%s", relativeUri);
+        String explicitUri = String.format("%s%s", viewUri, relativeUri);
         UiHost.getInstance().load(explicitUri);
     }
 
