a7f53b0680cc39e811769389342df90ba23f4472
Craig Stockton <cstockto@teksystems.com>
2018-06-10 10:51:36 -0700

    Refactoring to add javadocs and remove unnecessary code

--- src/main/java/com/softwareonpurpose/uinavigator/UiView.java
+++ src/main/java/com/softwareonpurpose/uinavigator/UiView.java
@@ -15,7 +15,6 @@
  */
 package com.softwareonpurpose.uinavigator;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
@@ -27,20 +26,37 @@ public abstract class UiView {
     private final UiElement viewElement;
     private final String viewUri;
 
+    /***
+     * Super constructor of UiView classes
+     * @param viewUri The full uri to the web page (view)
+     * @param viewElement The UiElement fully containing the displayed content of the web page (view)
+     */
     protected UiView(String viewUri, UiElement viewElement) {
         this.viewUri = viewUri;
         this.viewElement = viewElement;
     }
 
+    /***
+     * Called from any 'action' method which is expected to result in this view being displayed
+     * @param viewClass The UiView class for the view expected to be displayed
+     * @param <T> Any class extending UiView
+     * @return An instantiated UiView object representing the view expected to be displayed
+     */
     public static <T extends UiView> T expect(Class<T> viewClass) {
-        LoggerFactory.getLogger("").info(String.format("Expect '%s'", viewClass.getSimpleName().replaceAll(String
-                        .format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])")
-                , " ")));
+        String invalidCharacters =
+                String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])");
+        LoggerFactory.getLogger("").info(String.format("Expect '%s'", viewClass.getSimpleName().replaceAll(invalidCharacters, " ")));
         T view = construct(viewClass);
         view.confirmElementStates();
         return construct(viewClass);
     }
 
+    /***
+     * Construct a concrete UiView object
+     * @param viewClass The UiView class to be constructed
+     * @param <T> A class extending UiView
+     * @return An instantiated UiView object
+     */
     @SuppressWarnings("WeakerAccess")
     protected static <T extends UiView> T construct(Class<T> viewClass) {
         T view = null;
@@ -59,23 +75,35 @@ public abstract class UiView {
         return view;
     }
 
-    private static <T extends UiView> Logger getLogger(Class<T> viewClass) {
-        return LoggerFactory.getLogger(viewClass);
-    }
-
+    /***
+     * Includes everything necessary to confirm a concrete UiView is actually displayed
+     * @return boolean
+     */
     @SuppressWarnings({"WeakerAccess", "UnusedReturnValue"})
     protected abstract boolean confirmElementStates();
 
+    /***
+     * Navigate the UiHost (browser) to the uri for this concrete UiView
+     */
     protected void load() {
         UiHost.getInstance().load(viewUri);
     }
 
+    /***
+     * Navigate the UiHost (browser) to the uri for this concrete UiView, with appended relative uri or query string
+     * @param relativeUri A relative uri to extend this UiView uri OR a query string
+     */
     protected void load(String relativeUri) {
         relativeUri = relativeUri.substring(0, 1).equals("?") ? relativeUri : String.format("/%s", relativeUri);
         String explicitUri = String.format("%s%s", viewUri, relativeUri);
         UiHost.getInstance().load(explicitUri);
     }
 
+    /**
+     * The element containging the displayed content of the view (web page)
+     *
+     * @return The UiElement that fully contains the displayed content of the view (web page)
+     */
     protected UiElement getElement() {
         return viewElement;
     }
