a1e32e1036acc191d555fbadca8922620a700171
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-16 14:50:25 -0700

    Added unit testing

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebUiElementTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebUiElementTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 @Test
 public class WebUiElementTests {
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void terminate() {
         WebUiHost.quitInstance();
     }
@@ -22,5 +22,13 @@ public class WebUiElementTests {
         getBehavior.execute();
         Class actual = getBehavior.execute().getClass();
         Assert.assertEquals(actual, expected, "Failed to return the WebElement after it has been initialized");
+    }
+
+    @Test
+    public void testWaitUntilVisible_nonexistent() {
+        MockView.directNav();
+        WebUiElement element = WebUiElement.getInstance("element", UiLocatorType.ID, "nonexistent");
+        boolean actual = element.waitUntilVisible();
+        Assert.assertFalse(actual, "Failed to return false when element is nonexistent");
     }
 }
