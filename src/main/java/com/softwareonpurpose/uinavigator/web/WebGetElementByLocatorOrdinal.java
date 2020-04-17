3da6f69a2590133dd94d43eea1e849910dee5c7c
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-17 09:02:06 -0700

    Return null when list size is less than ordinal requested

--- src/main/java/com/softwareonpurpose/uinavigator/web/WebGetElementByLocatorOrdinal.java
+++ src/main/java/com/softwareonpurpose/uinavigator/web/WebGetElementByLocatorOrdinal.java
@@ -3,6 +3,8 @@ package com.softwareonpurpose.uinavigator.web;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class WebGetElementByLocatorOrdinal implements WebGetElementBehavior {
     private final By locator;
     private final Integer ordinal;
@@ -18,6 +20,7 @@ public class WebGetElementByLocatorOrdinal implements WebGetElementBehavior {
 
     @Override
     public WebElement execute() {
-        return WebUiHost.getInstance().findUiElements(locator).get(ordinal - 1);
+        final List<WebElement> elements = WebUiHost.getInstance().findUiElements(locator);
+        return elements.size() >= ordinal ? elements.get(ordinal - 1) : null;
     }
 }
