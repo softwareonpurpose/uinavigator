9612bf17bc414e2fb74973d3024f318f3ae74de3
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-17 10:03:38 -0700

    Added unit testing

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebGetElementByLocatorParentTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebGetElementByLocatorParentTests.java
@@ -1,6 +1,8 @@
 package com.softwareonpurpose.uinavigator.web;
 
+import com.softwareonpurpose.uinavigator.web.mock.MockView;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -32,6 +34,16 @@ public class WebGetElementByLocatorParentTests {
     public void testConstructor_nameLocatorNullParent() {
         Class expected = WebGetElementByLocatorParent.class;
         Class actual = WebGetElementByLocatorParent.getInstance(new By.ByName("user_name"), null).getClass();
+        final String message = "Failed to return new instance with parent 'null' and locator is 'body' tag";
+        Assert.assertEquals(actual, expected, message);
+    }
+
+    @Test
+    public void testExecute() {
+        MockView.directNav();
+        final By.ByName locator = new By.ByName("user_name");
+        Class expected = RemoteWebElement.class;
+        Class actual = WebGetElementByLocatorParent.getInstance(locator, null).execute().getClass();
         final String message = "Failed to return new instance with parent 'null' and locator is 'body' tag";
         Assert.assertEquals(actual, expected, message);
     }
