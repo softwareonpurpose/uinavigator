acdfe7f87c5065c2f7573aa9eacdc0cdf2c4605d
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-17 15:14:40 -0700

    Improved unit test coverage

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebGetElementByLocatorAttributeOrdinalNonExistentTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebGetElementByLocatorAttributeOrdinalNonExistentTests.java
@@ -2,29 +2,28 @@ package com.softwareonpurpose.uinavigator.web;
 
 import com.softwareonpurpose.uinavigator.web.mock.MockView;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test
-public class WebGetElementByLocatorAttributeOrdinalTests {
+public class WebGetElementByLocatorAttributeOrdinalNonExistentTests {
     @AfterMethod(alwaysRun = true)
     public void terminate() {
         WebUiHost.quitInstance();
     }
 
-    @Test(groups = "debug")
-    public void testExecute() {
+    @Test
+    public void testExecute_nonExistent() {
         MockView.directNav();
-        Class expected = RemoteWebElement.class;
         final By.ByTagName locator = new By.ByTagName("select");
         final String attribute = "data-test";
         final String attributeValue = "select-element";
-        final int ordinal = 2;
+        final int ordinal = 3;
         final WebGetElementByLocatorAttributeOrdinal getBehavior =
                 WebGetElementByLocatorAttributeOrdinal.getInstance(locator, attribute, attributeValue, ordinal);
-        Class actual = getBehavior.execute().getClass();
-        Assert.assertEquals(actual, expected, "Failed to return an instance of WebElement");
+        final WebElement actual = getBehavior.execute();
+        Assert.assertNull(actual, "Failed to return null when ordinal is nonexistent");
     }
 }
