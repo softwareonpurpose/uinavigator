ef8b9b3f925f34f61e8d775f528705e65612a67c
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-17 11:50:41 -0700

    Extract tests to improve unit test execution time

---
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebUiHostDriverRelatedTests.java
@@ -0,0 +1,29 @@
+package com.softwareonpurpose.uinavigator.web;
+
+import com.softwareonpurpose.uinavigator.web.mock.MockView;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+@Test
+public class WebUiHostDriverRelatedTests {
+    @AfterMethod(alwaysRun = true)
+    public void terminate() {
+        WebUiHost.quitInstance();
+    }
+
+    @Test
+    public void testGetUri() {
+        String expected = MockView.directNav().getUri();
+        String actual = WebUiHost.getInstance().getUri();
+        Assert.assertEquals(actual, expected, "Failed to return expected URI");
+    }
+
+    @Test
+    public void testGetDriverName() {
+        String expected = "ChromeDriver";
+        String actual = WebUiHost.getInstance().getDriverName();
+        Assert.assertTrue(actual.endsWith(expected), "Failed to return correct driver name");
+    }
+}
+
--- src/test/java/com/softwareonpurpose/uinavigator/web/WebUiHostHostRelatedTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebUiHostHostRelatedTests.java
@@ -1,6 +1,5 @@
 package com.softwareonpurpose.uinavigator.web;
 
-import com.softwareonpurpose.uinavigator.web.mock.MockView;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -17,20 +16,6 @@ public class WebUiHostHostRelatedTests {
         Class expected = WebUiHost.class;
         Class actual = WebUiHost.getInstance(DefaultChromeInstantiation.getInstance()).getClass();
         Assert.assertEquals(actual, expected, "Failed to return an instance of WebUiHost");
-    }
-
-    @Test
-    public void testGetUri() {
-        String expected = MockView.directNav().getUri();
-        String actual = WebUiHost.getInstance().getUri();
-        Assert.assertEquals(actual, expected, "Failed to return expected URI");
-    }
-
-    @Test
-    public void testGetDriverName() {
-        String expected = "ChromeDriver";
-        String actual = WebUiHost.getInstance().getDriverName();
-        Assert.assertTrue(actual.endsWith(expected), "Failed to return correct driver name");
     }
 }
 
