ec516ea21cfe2e0b3cb993cc2f8df586b9c92a91
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-17 11:21:08 -0700

    Extract tests to improve unit test execution time

--- src/test/java/com/softwareonpurpose/uinavigator/web/CookieViewerNameDomainQualifiedNameOnlyTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/CookieViewerNameDomainQualifiedNameOnlyTests.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test
-public class CookieViewerNameDomainQualifiedTests {
+public class CookieViewerNameDomainQualifiedNameOnlyTests {
     private WebDriver driver;
 
     @AfterMethod
@@ -15,19 +15,6 @@ public class CookieViewerNameDomainQualifiedTests {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    @Test
-    public void testGetCookieValue_nameDomainQualifiedMatch() {
-        driver = DefaultChromeInstantiation.getInstance().instantiateDriver();
-        String uri = "http://www.google.com";
-        driver.navigate().to(uri);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("document.cookie = \"cookiename=cookievalue\";");
-        String expected = "cookievalue";
-        CookieViewer viewer = CookieViewer.getInstance(driver);
-        String actual = viewer.getCookieValue("cookiename", "www.google.com");
-        Assert.assertEquals(actual, expected, "Failed to return expected cookie value");
     }
 
     @Test
