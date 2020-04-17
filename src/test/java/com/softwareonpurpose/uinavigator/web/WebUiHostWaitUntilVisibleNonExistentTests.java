4437016c12aaf1aba7e4b9cf658917b9cf7f6c54
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-04-17 11:25:19 -0700

    Extract tests to improve unit test execution time

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebUiHostWaitUntilVisibleNonExistentTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebUiHostWaitUntilVisibleNonExistentTests.java
@@ -8,18 +8,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test
-public class WebUiHostElementRelatedTests {
+public class WebUiHostWaitUntilVisibleNonExistentTests {
     @AfterMethod(alwaysRun = true)
     public void terminate() {
         WebUiHost.quitInstance();
-    }
-
-    @Test
-    public void testFindUiElement_nonexistent() {
-        WebElement expected = null;
-        WebElement actual = WebUiHost.getInstance().findUiElement(new By.ById("non-existent"));
-        //noinspection ConstantConditions
-        Assert.assertEquals(actual, expected, "Failed to return null for non-existent element");
     }
 
     @Test
