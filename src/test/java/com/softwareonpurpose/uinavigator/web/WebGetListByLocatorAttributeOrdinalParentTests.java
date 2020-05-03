ede39f85147aaadcea749e3ce23cf7756df6fdcf
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-05-02 23:04:44 -0700

    Added unit test for element with classname 'body'

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebGetListByLocatorAttributeOrdinalParentTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebGetListByLocatorAttributeOrdinalParentTests.java
@@ -29,7 +29,7 @@ public class WebGetListByLocatorAttributeOrdinalParentTests {
         Assert.assertEquals(actual, expected, "Failed to return a list of WebUiElement");
     }
 
-    @Test(groups = "debug")
+    @Test
     public void testExecute_bodyTag() {
         WebGetElementBehavior getParent = WebGetElementByLocator.getInstance(UiLocatorType.TAG, "form");
         Class<WebUiElement> expected = WebUiElement.class;
@@ -41,6 +41,23 @@ public class WebGetListByLocatorAttributeOrdinalParentTests {
         final WebGetListByLocatorAttributeOrdinalParent getBehavior =
                 WebGetListByLocatorAttributeOrdinalParent.getInstance(
                         UiLocatorType.TAG, locatorValue, attribute, attributeValue, ordinal, getParent);
+        //noinspection rawtypes
+        Class actual = getBehavior.execute().iterator().next().getClass();
+        Assert.assertEquals(actual, expected, "Failed to return a list of WebUiElement");
+    }
+
+    @Test
+    public void testExecute_bodyClass() {
+        WebGetElementBehavior getParent = WebGetElementByLocator.getInstance(UiLocatorType.TAG, "form");
+        Class<WebUiElement> expected = WebUiElement.class;
+        MockView.directNav();
+        final String attribute = "data-test";
+        final String attributeValue = "label-element";
+        final int ordinal = 1;
+        final String locatorValue = "body";
+        final WebGetListByLocatorAttributeOrdinalParent getBehavior =
+                WebGetListByLocatorAttributeOrdinalParent.getInstance(
+                        UiLocatorType.CLASS, locatorValue, attribute, attributeValue, ordinal, getParent);
         //noinspection rawtypes
         Class actual = getBehavior.execute().iterator().next().getClass();
         Assert.assertEquals(actual, expected, "Failed to return a list of WebUiElement");
