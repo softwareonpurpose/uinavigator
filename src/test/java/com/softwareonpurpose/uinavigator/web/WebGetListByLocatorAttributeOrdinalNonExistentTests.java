6df93e364629ebbea526c79c7fb814b14e4a82db
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-05-02 18:35:35 -0700

    Encapsulated unit test to improve execution time

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebGetListByLocatorAttributeOrdinalNonExistentTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebGetListByLocatorAttributeOrdinalNonExistentTests.java
@@ -6,24 +6,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test
-public class WebGetListByLocatorAttributeOrdinalTests {
+public class WebGetListByLocatorAttributeOrdinalNonExistentTests {
     @AfterMethod(alwaysRun = true)
     public void terminate() {
         WebUiHost.quitInstance();
     }
 
-    public void testExecute() {
+    public void testExecute_nonexistent() {
         MockView.directNav();
         final String attribute = "data-test";
         final String attributeValue = "select-element";
-        final int ordinal = 2;
+        final int ordinal = 3;
         final String locatorValue = "select";
         final WebGetListByLocatorAttributeOrdinal getBehavior =
-                WebGetListByLocatorAttributeOrdinal.getInstance(
-                        UiLocatorType.TAG, locatorValue, attribute, attributeValue, ordinal);
-        Class<WebUiElement> expected = WebUiElement.class;
-        //noinspection rawtypes
-        Class actual = getBehavior.execute().iterator().next().getClass();
-        Assert.assertEquals(actual, expected, "Failed to return an instance of WebElement");
+                WebGetListByLocatorAttributeOrdinal.getInstance(UiLocatorType.TAG, locatorValue, attribute, attributeValue, ordinal);
+        Integer expected = 0;
+        Integer actual = getBehavior.execute().size();
+        Assert.assertEquals(actual, expected, "Failed to return an empty list of WebUiElement");
     }
 }
