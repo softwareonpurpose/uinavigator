660b6d39e1642ff23935cb94e4f2c1ea4c122437
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-05-06 22:26:07 -0700

    Extracted load test to reduce execution time

--- src/test/java/com/softwareonpurpose/uinavigator/UiViewLoadTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/UiViewLoadTests.java
@@ -7,17 +7,17 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test
-public class UiViewTests {
+public class UiViewLoadTests {
     @AfterMethod
     public void terminate() {
         WebUiHost.quitInstance();
     }
 
     @Test
-    public void testExpect() {
+    public void testLoad_queryString() {
         //noinspection rawtypes
         Class expected = MockView.class;
-        UiView view = MockView.directNav();
+        UiView view = MockView.directNav("?name=value");
         //noinspection rawtypes
         Class actual = UiView.expect(view.getClass()).getClass();
         Assert.assertEquals(actual, expected, "Failed to return an instance of WebUiElement");
