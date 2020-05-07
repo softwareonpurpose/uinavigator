2aea9150c2a40e37900b66cabe734a8e7b436bec
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-05-06 22:19:09 -0700

    Added test setting element to null

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebUiElementBehaviorsSetTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebUiElementBehaviorsSetTests.java
@@ -20,7 +20,18 @@ public class WebUiElementBehaviorsSetTests {
         element.set("Hello World");
         String expected = "Hello World";
         String actual = element.getText();
-        WebUiHost.quitInstance();
+        String message = "Failed to return expected text";
+        Assert.assertEquals(actual, expected, message);
+    }
+
+    @Test
+    public void testSet_null() {
+        String uri = "file:///C:/Users/craig/Documents/git/uinavigator/src/test/resources/MockPage.html";
+        WebUiHost.getInstance().load(uri);
+        WebUiElement element = WebUiElement.getInstance("Element", UiLocatorType.NAME, "user_name");
+        element.set(null);
+        String expected = "";
+        String actual = element.getText();
         String message = "Failed to return expected text";
         Assert.assertEquals(actual, expected, message);
     }
