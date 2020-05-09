79a1beec1c0adf8d0ff08c02f0b8354a8394c125
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-05-08 18:20:02 -0700

    Extracted set element to null tests to reduce execution time

--- src/test/java/com/softwareonpurpose/uinavigator/web/WebUiElementBehaviorsSetNullTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/web/WebUiElementBehaviorsSetNullTests.java
@@ -6,19 +6,19 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Test
-public class WebUiElementBehaviorsSetTests {
+public class WebUiElementBehaviorsSetNullTests {
     @AfterMethod
     public void terminate() {
         WebUiHost.quitInstance();
     }
 
     @Test
-    public void testSet() {
+    public void testSet_null() {
         String uri = "file:///C:/Users/craig/Documents/git/uinavigator/src/test/resources/MockPage.html";
         WebUiHost.getInstance().load(uri);
         WebUiElement element = WebUiElement.getInstance("Element", UiLocatorType.NAME, "user_name");
-        element.set("Hello World");
-        String expected = "Hello World";
+        element.set(null);
+        String expected = "";
         String actual = element.getText();
         String message = "Failed to return expected text";
         Assert.assertEquals(actual, expected, message);
