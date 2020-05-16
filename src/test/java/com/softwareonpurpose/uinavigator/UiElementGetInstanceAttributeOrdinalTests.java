d9cd9be123b2b963e44bf3f6fcedc5884ffaebb1
Craig A. Stockton <craig.a.stockton@gmail.com>
2020-05-16 08:08:04 -0700

    Encapsulated unit tests to improve execute run time

---
+++ src/test/java/com/softwareonpurpose/uinavigator/UiElementGetInstanceAttributeOrdinalTests.java
@@ -0,0 +1,25 @@
+package com.softwareonpurpose.uinavigator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class UiElementGetInstanceAttributeOrdinalTests {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void testGetInstance_attributeOrdinal() {
+        UiHost host = UiHost.getInstance();
+        final String description = "Select";
+        final String locatorType = UiLocatorType.TAG;
+        final String locatorValue = "option";
+        final String attribute = "data-test";
+        final String attributeValue = "select-element";
+        Integer ordinal = 2;
+        Class expected = UiElement.class;
+        final UiElement element =
+                UiElement.getInstance(description, locatorType, locatorValue, attribute, attributeValue, ordinal, host);
+        Class actual = element.getClass();
+        host.quit();
+        Assert.assertEquals(actual, expected, "Failed to return an instance of WebUiElement");
+    }
+}
