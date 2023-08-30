eb777d7d6bfaba84870d553540e12d20b4b27d5d
tldav <tldavis09@yahoo.com>
2023-08-30 14:53:25 -0500

    Extracted getElement functionality to UiElement

--- src/main/java/com/softwareonpurpose/uinavigator/UiElement4.java
+++ src/main/java/com/softwareonpurpose/uinavigator/UiElement4.java
@@ -1,11 +1,23 @@
 package com.softwareonpurpose.uinavigator;
 
-public class UiElement4 {
-    public static UiElement4 getInstance(String description, String locatorType, String locatorValue) {
-        return new UiElement4();
-    }
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class UiElement4 {
+    private final By.ByCssSelector locator;
+    private final ChromeDriver driver = UiNavigator.getDriver();
+    
+    public UiElement4(String description, String locatorType, String locatorValue) {
+        String cssSymbol = "#";
+        String css = String.format("%s%s", cssSymbol, locatorValue);
+        locator = new By.ByCssSelector(css);
+    }
+    
+    public static UiElement4 getInstance(String description, String locatorType, String locatorValue) {
+        return new UiElement4(description, locatorType, locatorValue);
+    }
+    
     public boolean isDisplayed() {
-        return false;
+        return driver.findElement(locator).isDisplayed();
     }
 }
--- src/main/java/com/softwareonpurpose/uinavigator/UiNavigator.java
+++ src/main/java/com/softwareonpurpose/uinavigator/UiNavigator.java
@@ -1,26 +1,25 @@
 package com.softwareonpurpose.uinavigator;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UiNavigator {
     private static ChromeDriver driver = new ChromeDriver(new ChromeOptions());
-
+    
     public static void quitInstance() {
         driver.quit();
         driver = null;
     }
-
+    
     public static void load(String url) {
         driver.get(url);
     }
-
-    public static WebElement getElement(String description, String locatorType, String locatorValue) {
-        String cssSymbol = "#";
-        String css = String.format("%s%s", cssSymbol, locatorValue);
-        By locator = new By.ByCssSelector(css);
-        return driver.findElement(locator);
+    
+    public static UiElement4 getElement(String description, String locatorType, String locatorValue) {
+        return UiElement4.getInstance(description, locatorType, locatorValue);
+    }
+    
+    public static ChromeDriver getDriver() {
+        return driver;
     }
 }
