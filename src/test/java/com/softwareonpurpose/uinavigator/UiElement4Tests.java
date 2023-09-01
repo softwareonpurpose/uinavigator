4ad288e4cc240e1a12de9d565309d1544efb3090
tldav <tldavis09@yahoo.com>
2023-09-01 11:30:49 -0500

    Replaced absolute path to html with relative url in UiElementTests

--- src/test/java/com/softwareonpurpose/uinavigator/UiElement4Tests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/UiElement4Tests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+@SuppressWarnings("ConstantConditions")
 @Test
 public class UiElement4Tests {
     @DataProvider
@@ -16,19 +17,19 @@ public class UiElement4Tests {
                         , {"p"}
                 };
     }
-
+    
     @AfterMethod
     public void terminate() {
         UiNavigator.getInstance().quitDriver();
     }
-
+    
     @Test(dataProvider = "tags")
     public void isDisplayed(String tag) {
-        ;
-        String url = "file:///D:/git/uinavigator/src/test/resources/basic.html";
+        String url = getClass().getResource("/basic.html").toString();
         UiHost4.getInstance().load(url);
         boolean expected = true;
         boolean actual = UiElement4.getInstance("'Tutorials' nav button", UiLocatorType4.TAG, tag).isDisplayed();
         Assert.assertEquals(actual, expected);
     }
+    
 }
