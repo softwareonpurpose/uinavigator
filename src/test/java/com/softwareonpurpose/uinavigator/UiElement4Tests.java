d5e19098628125a623de6c02f7675a169c858108
tldav <tldavis09@yahoo.com>
2023-09-06 19:24:48 -0500

    Added scenario to find by id using parent element to isDisplayed()

--- src/test/java/com/softwareonpurpose/uinavigator/UiElement4Tests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/UiElement4Tests.java
@@ -22,6 +22,10 @@ public class UiElement4Tests {
     private static final UiElement4 breakElement = UiElement4.getInstance("'break' element", UiLocatorType4.TAG, "br");
     private static final UiElement4 preElement = UiElement4.getInstance("'pre' element", UiLocatorType4.TAG, "pre");
     private static final UiElement4 paragraphElement_4 = UiElement4.getInstance("'paragraph' element", UiLocatorType4.TAG, "p", 4);
+    private static final UiElement4 headingElementById = UiElement4.getInstance("'heading' element", UiLocatorType4.ID, "heading-id");
+    private static final UiElement4 elementByInvalidId = UiElement4.getInstance("invalid id", UiLocatorType4.ID, "non-existent-id");
+    private static final UiElement4 divElementById = UiElement4.getInstance("'div' element", UiLocatorType4.ID, "div-id");
+    private static final UiElement4 nestedParagraphElementById = UiElement4.getInstance("'paragraph' element", UiLocatorType4.ID, "p-id", divElementById);
     private static final String basicPage = "basic";
     private static final String paragraphsPage = "paragraphs";
     private static final String linkPage = "link";
@@ -32,8 +36,6 @@ public class UiElement4Tests {
     private static final String prePage = "pre";
     private static final String stylePage = "style";
     private static final String idPage = "id";
-    private static final UiElement4 headingElementById = UiElement4.getInstance("'heading' element", UiLocatorType4.ID, "myHeader");
-    private static final UiElement4 elementInvalidId = UiElement4.getInstance("invalid id", UiLocatorType4.ID, "invalid");
     
     @DataProvider
     public static Object[][] scenarios_isDisplayed() {
@@ -52,7 +54,8 @@ public class UiElement4Tests {
                         , {breakPage, breakElement, isNotDisplayed}
                         , {prePage, preElement, isDisplayed}
                         , {idPage, headingElementById, isDisplayed}
-                        , {idPage, elementInvalidId, isNotDisplayed}
+                        , {idPage, elementByInvalidId, isNotDisplayed}
+                        , {idPage, nestedParagraphElementById, isDisplayed}
                 };
     }
 
