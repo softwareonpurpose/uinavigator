b4a6f35d0b7ba471018624deed4d2c8b1fbb0c00
Craig A. Stockton <craig.a.stockton@gmail.com>
2023-09-30 00:49:02 -0700

    Changed return type of 'creation' method in GetWebElementBehavior to better align with a 'Factory' approach

--- src/main/java/com/softwareonpurpose/uinavigator/GetWebElementBehavior.java
+++ src/main/java/com/softwareonpurpose/uinavigator/GetWebElementBehavior.java
@@ -3,7 +3,7 @@ package com.softwareonpurpose.uinavigator;
 import org.openqa.selenium.WebElement;
 
 public abstract class GetWebElementBehavior {
-    public static GetByCssFromRoot getInstance(String locatorType, String locatorValue) {
+    public static GetWebElementBehavior getInstance(String locatorType, String locatorValue) {
         return new GetByCssFromRoot(locatorType, locatorValue);
     }
 
