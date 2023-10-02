a872510c2715fa0fd9df697ce7b3123dab7ec44f
Craig A. Stockton <craig.a.stockton@gmail.com>
2023-10-02 11:30:46 -0700

    Extracted 'action' tests to separate test file

---
+++ src/test/java/com/softwareonpurpose/uinavigator/UiElement4ActionTests.java
@@ -0,0 +1,42 @@
+package com.softwareonpurpose.uinavigator;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
+public class UiElement4ActionTests {
+    private static final TestResources resources = TestResources.getInstance();
+
+    @DataProvider
+    public static Object[][] scenarios_click() {
+        final UiElement4 byTagP =
+                UiElement4.getInstance("tag 'p'", UiLocatorType4.TAG, "p");
+        final UiElement4 byTagA =
+                UiElement4.getInstance("'Anchor' tag", UiLocatorType4.TAG, "a");
+        final String basicPage = "basic";
+        final String linkPage = "link";
+        return new Object[][]{
+                {basicPage, byTagP, "basic"}
+                , {basicPage, byTagA, "basic"}
+                , {linkPage, byTagA, "https://www.w3schools.com/"}
+        };
+    }
+
+    @AfterMethod
+    public void terminate() {
+        UiNavigator.getInstance().quitDriver();
+    }
+
+    @Test(dataProvider = "scenarios_click")
+    public void click(String page, UiElement4 element, String expected) {
+        expected = expected.contains("http")
+                ? expected
+                : resources.getPageUrl(expected).replace("file:/", "file:///");
+        UiHost4.getInstance().load(resources.getPageUrl(page));
+        element.click();
+        String actual = UiHost4.getInstance().getCurrentUrl();
+        Assert.assertEquals(actual, expected);
+    }
+}
