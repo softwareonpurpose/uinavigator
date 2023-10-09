7d774d2ecd5e515f7e8d1c52749b95bedb1cc951
Craig A. Stockton <craig.a.stockton@gmail.com>
2023-10-09 14:01:16 -0700

    Extracted UiFrameTest.isDisplayed scenarios

--- src/test/java/com/softwareonpurpose/uinavigator/UiFrameTests.java
+++ src/test/java/com/softwareonpurpose/uinavigator/UiFrameTests.java
@@ -1,16 +1,23 @@
 package com.softwareonpurpose.uinavigator;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test
 public class UiFrameTests {
     private static final TestResources resources = TestResources.getInstance();
 
-    @Test
-    public void isDisplayed() {
-        boolean expected = false;
-        boolean actual = UiFrame.getInstance().isDisplayed();
+    @DataProvider
+    public static Object[][] scenarios_isDisplayed() {
+        return new Object[][]{
+                {UiFrame.getInstance(), false}
+        };
+    }
+
+    @Test(dataProvider = "scenarios_isDisplayed")
+    public void isDisplayed(UiFrame frame, boolean expected) {
+        boolean actual = frame.isDisplayed();
         Assert.assertEquals(actual, expected);
     }
 }
